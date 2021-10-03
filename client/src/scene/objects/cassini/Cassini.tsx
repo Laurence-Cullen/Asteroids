3d566522b9c5766aeeaa459e25164c7431a03764
James Paterson <jpaterson@tripadvisor.com>
2021-10-03 17:15:21 +0100

    voxeroid

--- client/src/scene/objects/cassini/Cassini.tsx
+++ client/src/scene/objects/cassini/Cassini.tsx
@@ -3,17 +3,22 @@ import {GLTFModelLoader} from "../../model-loader/GLTFModelLoader";
 import {Box} from "../box/Box";
 import {Euler} from "three";
 
+const CAMERA_POSITION: [number, number, number] = [0, 50, 0];
+
+/**
+ * Cassini is the "camera" for the scene, and where the images of the asteroid are captured from
+ */
 const Cassini: React.FC<{}> = () => {
 
     return (
-        <Suspense fallback={<Box position={[0, 50, 0]} />}>
+        <Suspense fallback={<Box position={CAMERA_POSITION} />}>
             <GLTFModelLoader
                 filename={'./spacecraft/cassini.gltf'}
-                position={[0, 50, 0]}
-                rotation={new Euler(Math.PI, 0, 0)}
+                position={CAMERA_POSITION}
+                rotation={new Euler(Math.PI, 0, 0)} // Rotate it so it points at the object, so it looks nice :)
             />
         </Suspense>
     )
 }
 
-export { Cassini };
+export { Cassini, CAMERA_POSITION };
